172bcf6cf4155e18b9fcf59f45c6be272c813344
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-08-19 14:55:35 -0700

    installed webpack plugin

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 var app = new Vue({
-  el: '#app',
-  data() {
-    return {
-      message: 'Vuepoints',
-      value: 0,
-      username: '',
-      page: 0
-    }
+    el: '#app',
+    data() {
+        return {
+            message: 'Hailey',
+            value: 0,
+            username: '',
+            page: 0
+        };
 
-  },
-  methods: {
-    increment() {
-      this.value += 1;
     },
-    decrement() {
-      this.value -= 1;
-    },
-    submit() {
-      console.log(this.username);
-    },
-    nextPage() {
-      this.page += 1;
+    methods: {
+        increment() {
+            this.value += 1;
+        },
+        decrement() {
+            this.value -= 1;
+        },
+        submit() {
+            console.log(this.username);
+        },
+        nextPage() {
+            this.page += 1;
 
-      if (this.page >= 3) {
-        this.page = 0;
-      }
+            if (this.page >= 3) {
+                this.page = 0;
+            }
+        }
     }
-  }
-})
+});
 
